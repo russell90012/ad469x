714e291ca2cd5f67718178bdfc01beee6f5cd9e9
rshaheni <122036550+russell90012@users.noreply.github.com>
2023-04-10 15:41:26 +0000

    Stubbed out 'no_os' module.  Built.

--- .ccls-cache/@home@runner@ad469x/no_os.c
+++ .ccls-cache/@home@runner@ad469x/no_os.c
@@ -1,5 +1,65 @@
-#ifndef  NO_OS_H
-#define  NO_OS_H
+#include "no_os.h"
+
+int32_t no_os_gpio_get_optional(struct no_os_gpio_desc** gpio_param, struct no_os_gpio_init_param* gpio_param_init)
+{
+  return 0;
+}
+
+int32_t no_os_gpio_direction_output(struct no_os_gpio_desc* gpio_param, enum no_os_enums gpio_param_direction)
+{
+  return 0;
+}
 
 
-#endif  // NO_OS_H
+int32_t no_os_gpio_direction_input(struct no_os_gpio_desc* gpio_param)
+{
+  return 0;
+}
+
+
+	
+int32_t no_os_spi_write_and_read(struct no_os_spi_desc* spi_desc, uint32_t *buf, uint16_t samples)
+{
+  return 0;
+}
+
+uint8_t no_os_hweight16(uint16_t ch_mask)
+{
+  return 0;
+}
+
+int32_t no_os_gpio_set_value(struct no_os_gpio_desc *gpio_resetn, enum no_os_enums gpio_param_value)
+{
+  return 0;
+}
+
+void no_os_mdelay(uint32_t delay)
+{
+  
+}
+
+void* no_os_malloc(uint32_t size)
+{
+  return 0;
+}
+
+
+int32_t no_os_spi_init(struct no_os_spi_desc* spi_desc, struct no_os_spi_init_param* spi_init)
+{
+  return 0;
+}
+
+int32_t no_os_spi_remove(struct no_os_spi_desc* spi_desc)
+{
+  
+}
+
+int32_t no_os_gpio_remove(struct no_os_gpio_desc* gpio_param)
+{
+  return 0;
+}
+
+int32_t no_os_free(void* blob)
+{
+  return 0;
+}
--- .ccls-cache/@home@runner@ad469x/no_os.h
+++ .ccls-cache/@home@runner@ad469x/no_os.h
@@ -2,6 +2,8 @@
 #ifndef  NO_OS_H
 #define  NO_OS_H
 
+#include <stdint.h>
+
 enum no_os_enums
 {
   NO_OS_GPIO_LOW = 0,
@@ -12,14 +14,51 @@ enum no_os_enums
 
 struct no_os_gpio_desc
 {
+  
 };
 
-int32_t no_os_gpio_get_optional(struct no_os_gpio_desc** gpio_resetn, struct no_os_gpio_desc* gpio_resetn);
+struct no_os_gpio_init_param
+{
+  
+};
+
+struct no_os_spi_desc
+{
+  
+};
 
 
-int32_t no_os_gpio_get_optional(struct no_os_gpio_desc** gpio_busy,  struct no_os_gpio_desc* gpio_busy);
+struct no_os_spi_init_param
+{
 
-int32_t no_os_gpio_direction_output(struct no_os_gpio_desc* gpio_resetn, no_os_enums direction_value);
-int32_t no_os_gpio_direction_input(struct no_os_gpio_desc* gpio_busy);
+};
+
+int32_t no_os_gpio_get_optional(struct no_os_gpio_desc** gpio_param, struct no_os_gpio_init_param* gpio_param_init);
+int32_t no_os_gpio_direction_output(struct no_os_gpio_desc* gpio_param, enum no_os_enums gpio_param_value);
+int32_t no_os_gpio_direction_input(struct no_os_gpio_desc* gpio_param);
 	
+int32_t no_os_spi_write_and_read(struct no_os_spi_desc* spi_desc, uint32_t *buf, uint16_t samples);
+
+uint8_t no_os_hweight16(uint16_t ch_mask);
+
+int32_t no_os_gpio_set_value(struct no_os_gpio_desc* gpio_resetn, enum no_os_enums gpio_param_value);
+
+
+void no_os_mdelay(uint32_t);
+
+
+void* no_os_malloc(uint32_t size);
+
+int32_t no_os_spi_init(struct no_os_spi_desc* spi_desc, struct no_os_spi_init_param* spi_init);
+
+
+int32_t no_os_spi_remove(struct no_os_spi_desc* spi_desc);
+
+
+int32_t no_os_gpio_remove(struct no_os_gpio_desc* gpio_param);
+
+int32_t no_os_free(void* blob);
+
+
+
 #endif
